54959a2221a1ad2faea53156fdc2c805d8b21bc0
Jay <jaymanale29@gmail.com>
2016-04-22 08:11:41 +0530

    Forth changes.. adding new files dashboard

---
+++ app/src/main/java/com/teamtreehouse/parseworkshop/Dashboard.java
@@ -0,0 +1,15 @@
+package com.teamtreehouse.parseworkshop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.teamtreehouse.readme.R;
+
+
+public class Dashboard extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
